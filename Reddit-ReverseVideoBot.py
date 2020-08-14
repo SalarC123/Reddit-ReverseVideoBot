e376b68ca3bd9c4298e4e2550cd63cc825bd0702
Tabinda <Tabinda@chaudrys-iMac.local>
2020-08-14 12:16:52 -0500

    Make an outline for the project with four functions and a TODO list to organize code

--- Reddit-ReverseVideoBot.py
+++ Reddit-ReverseVideoBot.py
@@ -1,9 +1,39 @@
 import praw
+from botsecrets2 import *
+import os
+import urllib
 
+
+os.chdir('/users/tabinda/documents/python/reddit-reversevideobot')
+
+reddit = praw.Reddit(username=PUT_USER_NAME_HERE,
+                     password=PUT_PASSWORD_HERE,
+                     client_id=PUT_CLIENT_ID_HERE,
+                     client_secret=PUT_CLIENT_SECRET_HERE,
+                     user_agent=PUT_USER_AGENT_HERE)
+
+def bot_run():
+    pass
+
+def find_link_and_download(redditsubmission):
+    pass
+
+def reverse_video_and_save(videofile):
+    pass
+
+def post_video(videolink/download???):
+    pass
+
+
+
+
+
+#TODO Checklist
 '''
-take reddit link
-add .json to end
-index for fallback_url to get video link
-use yotubedl to manipulate maybe
-use open-cv or other module to reverse
+multiple functions
+refactor
+multiline comments
+comments throughout code
+commit often - committing this change will...
+inspiration --> https://sandiegofreepress.org/2017/11/one-veterans-dream-kurt-vonneguts-war-in-reverse-video-worth-watching/#.XzbBwS2z2Dc
 '''
